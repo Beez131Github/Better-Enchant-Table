82a6f7af2728a75cb386948944ee90f3aee601fa
Beez131Github <olivercollins123@hotmail.com>
2025-01-17 13:34:15 +1300

    fabric api fixes, thanks to supersaiyansubtlety for helping

--- src/main/java/io/github/beez131github/betr_table/BetrTable.java
+++ src/main/java/io/github/beez131github/betr_table/BetrTable.java
@@ -1,15 +1,19 @@
 package io.github.beez131github.betr_table;
 
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BetrTable implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("Better Enchant Table");
 
-    @Override
     public void onInitialize(ModContainer mod) {
         LOGGER.info("Hello Quilt world from {}! Stay fresh!", mod.metadata().name());
     }
+
+	@Override
+	public void onInitialize() {
+
+	}
 }
